ef3243c85f5b5765c37adf556fc85fc8efde87fb
peterszabo79 <peterszabo79@gmail.com>
2022-04-12 20:52:01 +0000

    tidy my code after checked by pep8

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 import time
 # source from inventwithpython.com
@@ -41,29 +38,31 @@ HANGMAN_PICS = ['''
  / \  |
      ===''']
 
-# Add words to the game 
+# Add words to the game
 words = 'chthonic phlegm pterodactyl muscle dilate indict mnemonic\
      liquefy asthma apropos receipt knead nauseous honest'.split()
 
 # welcome the user
 name = input("What is Your name?")
 print ("Hello", name.capitalize(), "let's start \
-playing Hangman!")#name will start with uppercase letter always
+playing Hangman!").  # name will start with uppercase letter always
 time.sleep(1)
 
 print("The objective of the game is ,\
 to guess the secret word chosen by the computer.")
 time.sleep(2)
 
-print("You can guess only one letter at a time.\
-Please use only lowercase letters! Don't forget to press 'enter key' after each guess.")
+print("You can guess only one letter at a time.Please use only lowercase letters!\
+Don't forget to press 'enter key' after each guess.")
 time.sleep(3)
 
 print("You have 6 lifes.It's mean, You can miss 6 times.")
 time.sleep(2)
+
+
 # This function returns a random word from the passed list of strings
 def getRandomWord(wordList):
-    wordIndex = random.randint(0, len(wordList) -1)
+    wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
 
@@ -71,33 +70,37 @@ def displayBoard(missedLetters, correctLetters, secretWord):
 
     """
     this function defines a  displayBoard. it has three parameters:
-    missedLetters A string of the letters the player has guessed that are not in the secret word
-    correctLetters A string of the letters the player has guessed that are in the secret word
-    secretWord A string of the secret word that the player is trying to guess
-    
-    """  
+    missedLetters:A string of the letters the player has guessed
+    that are not in the secret word
+    correctLetters:A string of the letters the player has guessed
+    that are in the secret word
+    secretWord:A string of the secret word that the player is trying to guess
 
-    print(HANGMAN_PICS[len(missedLetters)]) # call hangman pics after user choice
-    print() 
+    """
+    # call hangman pics after user choice
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
     print('Missed letters:', end=' ')
     for letter in missedLetters:
         print(letter, end=' ')
     print()
     blanks = '_' * len(secretWord)
-
-    for i in range(len(secretWord)): # replace blanks with correctly guessed letters
+    # replace blanks with correctly guessed letters
+    for i in range(len(secretWord)):
         if secretWord[i] in correctLetters:
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
-    for letter in blanks: # show the secret word with spaces in between each letter
-        print(letter,end=' ')
+    # show the secret word with spaces in between each letter
+    for letter in blanks:
+        print(letter, end=' ')
     print()
 
-#show the letter what user entered
+
+# show the letter what user entered
 def getGuess(alreadyGuessed):
     while True:
         print('Guess a letter.')
         guess = input()
-        if len(guess) !=1:
+        if len(guess) != 1:
             print('Please enter a single letter.')
         elif guess in alreadyGuessed:
             print('You have already guessed that letter. Choose again.')
@@ -105,7 +108,14 @@ def getGuess(alreadyGuessed):
             print('Please enter a LOWERCASE-LETTER.')
         else:
             return guess
-#playAgain function - musst return true if user wants to play again, otherwise false           
+
+"""
+playAgain function:must return true if user wants to play again,
+otherwise false
+
+"""
+
+
 def playAgain():
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
@@ -130,18 +140,21 @@ while True:
                 foundAllLetters = False
                 break
         if foundAllLetters:
-            print('Yes', name.capitalize(),'the secret word is "' + secretWord + '"! Yay! you won!!!')
+            print('Yes', name.capitalize(),
+                  'the secret word is "' + secretWord + '"! Yay! you won!!!')
             gameIsDone = True
     else:
         missedLetters = missedLetters + guess
 
-        #Check if user guessed too many times and lost
+        # Check if user guessed too many times and lost
         if len(missedLetters) == len(HANGMAN_PICS) - 1:
             displayBoard(missedLetters, correctLetters, secretWord)
-            print('You have run out of guesses!\nSorry ', name.capitalize(), ' better luck next time.\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
+            print('You have run out of guesses!Sorry ', name.capitalize(),
+                  ' better luck next time.After ' + str(len(missedLetters)) +
+                  ' missed guesses and ' + str(len(correctLetters)) +
+                  ' correct guesses, the word was "' + secretWord + '"')
             gameIsDone = True
-    
-    #Ask user if they want to play again (but only if the game is done).
+    # Ask user if they want to play again (but only if the game is done).
     if gameIsDone:
         if playAgain():
             missedLetters = ''
